8f005b10ca2734e4b3468fc0cbb9e9ffa7392725
ahmetcaggn <ahmetcannersoy@gmail.com>
2024-12-24 21:15:42 +0300

    feat: Video uploading functionality has been added

--- api/src/main/java/com/nextStream/api/repository/PlaylistRepository.java
+++ api/src/main/java/com/nextStream/api/repository/PlaylistRepository.java
@@ -1,4 +1,7 @@
 package com.nextStream.api.repository;
 
+import com.nextStream.api.entity.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 }
