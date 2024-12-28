e4a42cb6d0917c4e66e368ae3c16b4598687d52a
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-12-29 00:22:28 +0300

    style: name change

--- next_stream_mobile/lib/product/route/app_routes.dart
+++ next_stream_mobile/lib/product/route/app_routes.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:next_stream_mobile/feature/home/screen_home.dart';
 import 'package:next_stream_mobile/feature/home/view_model/cubit_home.dart';
-import 'package:next_stream_mobile/feature/viewer/screen_upload.dart';
+import 'package:next_stream_mobile/feature/viewer/screen_viewer.dart';
 import 'package:next_stream_mobile/feature/viewer/view_model/cubit_viewer.dart';
 import 'package:next_stream_mobile/product/logger/log.dart';
 
 /// The manager class for the application routes.
 final class AppRoutes {
   static const String home = '/';
-  static const String viewer = '/upload';
+  static const String viewer = '/viewer';
 
   /// Generates the routes for the application. Uses in the
   /// [MaterialApp.onGenerateRoute] property.
